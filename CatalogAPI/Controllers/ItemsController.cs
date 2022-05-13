9bbd5d7d2cce6bec66d262563cf0ce3c8d0b2137
amanajames <idakwojijames@gmail.com>
2022-05-13 17:07:35 +0100

    Add project files.

---
+++ CatalogAPI/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using CatalogApi.Model;
+using CatalogApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CatalogApi.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemitemrepository _inMemItems;
+
+        public ItemsController()
+        {
+            _inMemItems = new InMemitemrepository();
+        }
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = _inMemItems.GetItems();
+            return items;
+        }
+    }
+}
---
+++ CatalogAPI/Repository/InMemitemrepository.cs
@@ -0,0 +1,27 @@
+﻿using CatalogApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CatalogApi.Repository
+{
+    public class InMemitemrepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = System.Guid.NewGuid(), Name = "potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = System.Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = System.Guid.NewGuid(), Name = "Bronze Shield", Price = 15, CreatedDate = DateTimeOffset.UtcNow },
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid Id)
+        {
+            return items.Where(item => item.Id == Id).SingleOrDefault();
+        }
+    }
+}
